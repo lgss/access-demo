d24dbdf247ae5cd356b8e01c46077ef59e323dbe
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2020-09-08 13:35:24 +0100

    amendments to img

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
       <article>
         <h2>How not to do a review of a service</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        <img href="assets/img1.jpg" />
+        <img src="./assets/img1.jpg" alt="sign stating no stupid people beyond this point"/>
         <a href="#">Author Name</a>
       </article>
       <article>
